dd81fa20c4f9d1da3bc9ec37cff406c5fc545e15
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-08-14 08:46:13 +0200

    Remove neo-tree config and update floating diagnostics window

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -174,19 +174,6 @@ return {
   },
 
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      disable_auto_open = true,
-      filesystem = {
-        filtered_items = {
-          visible = true, -- Show hidden files
-          hide_dotfiles = false, -- Do not hide dotfiles
-        },
-      },
-    },
-  },
-
-  {
     "iamcco/markdown-preview.nvim",
     config = function()
       vim.fn["mkdp#util#install"]()
