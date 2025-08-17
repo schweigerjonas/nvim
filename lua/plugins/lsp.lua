fb30f4c3c65b8c129c84859f301fe49a86253ec7
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-08-17 13:30:34 +0200

    Fix arduino-language-server not loading for .ino files
    **Important**:
    This solution has the board type hard coded inside the lsp.lua file.
    In order to use the lsp for other boards in the future, I suppose
    this has to be always changed to the currently used board.
    
    [Reference](https://www.reddit.com/r/neovim/comments/1h1hfk9/comment/mlexoom/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,7 +16,34 @@ return {
         "angular-language-server",
         "gopls",
         "arduino-language-server",
+        "clangd",
       })
     end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        arduino_language_server = {
+          cmd = {
+            "arduino-language-server",
+            "-cli-config",
+            vim.fn.expand("~/.arduino15/arduino-cli.yaml"),
+            "-fqbn",
+            "arduino:avr:uno",
+          },
+          capabilities = {
+            textDocument = {
+              semanticTokens = vim.NIL,
+            },
+            workspace = {
+              semanticTokens = vim.NIL,
+            },
+          },
+        },
+        clangd = {},
+      },
+    },
   },
 }
