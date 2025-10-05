c47900cdcd8b37ba803409a2d36251a4280c3b69
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-10-05 10:28:38 +0200

    Update LazyVim command line appearance

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,7 +9,14 @@ return {
         },
         opts = { skip = true },
       })
-      opts.presets.lsp_doc_border = true
+      -- classic command line instead of pop-up
+      opts.cmdline = {
+        view = "cmdline", -- move command line to bottom
+      }
+      opts.presets = {
+        lsp_doc_border = true,
+        command_palette = false, -- tab completions for command line don't pop-up at top
+      }
     end,
   },
 
