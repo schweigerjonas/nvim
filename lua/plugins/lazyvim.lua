98457725ae5900316a994912b9cf9a048adcb698
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-10-05 09:56:11 +0200

    Pin LazyVim to version 14

---
+++ lua/plugins/lazyvim.lua
@@ -0,0 +1,4 @@
+return {
+  -- pin to LazyVim version 14
+  { "LazyVim/LazyVim", version = "^14" },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -61,7 +61,7 @@ return {
 
   -- Animations
   {
-    "echasnovski/mini.animate",
+    "nvim-mini/mini.animate",
     event = "VeryLazy",
     opts = function(_, opts)
       opts.scroll = {
